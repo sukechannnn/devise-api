0bcf90d70f1e8f067b67831a27ff10e06f8204b4
yosuke0315 <wonderfulworld0315@gmail.com>
2016-05-31 21:23:48 +0900

    テストでI18nを適用 & tiny fix

--- spec/requests/users_registrations_spec.rb
+++ spec/requests/users_registrations_spec.rb
@@ -8,48 +8,48 @@ RSpec.describe 'Users Registrations', type: :request do
     { user: { email: 'username+1@basicinc.jp', password: 'password', service: 4 } }
   end
 
-  context 'POST /users (ユーザー登録) when no need mail registration ' do
+  context 'POST /users (ユーザー登録) when no need mail registration' do
     it '登録完了' do
       post '/users', user_params
       rsa_public = OpenSSL::PKey.read ENV['RSA_PUBLIC']
       session_data = JWT.decode JSON.parse(response.body)['token'], rsa_public, true, algorithm: 'RS256'
       expect(response.status).to eq 200
       p session_data.first.deep_symbolize_keys
-      expect(session_data.first.deep_symbolize_keys[:email]).to eq 'username+1@basicinc.jp'
+      expect(session_data.first.deep_symbolize_keys[:email]).to eq user_params[:user][:email]
       expect(response).to match_response_schema('/users')
     end
   end
 
-  context 'POST /users (ユーザー登録) when need mail registration ' do
+  context 'POST /users (ユーザー登録) when need mail registration' do
     it '本登録が必要なこと' do
       post '/users', user_params.deep_merge(user: { service: 2 })
       expect(response.status).to eq 200
-      expect(flash[:alert]).to be_include '本登録を行ってください。'
+      expect(flash[:alert]).to be_include I18n.t 'devise.failure.unconfirmed'
       expect(User.first.service).to eq 2
       expect(User.first.confirmed_at.nil?).to eq true
     end
   end
 
-  context 'POST /users (ユーザー登録) when need mail registration ' do
+  context 'POST /users (ユーザー登録) when need mail registration' do
     it '本登録が必要でログイン出来ないこと' do
       post '/users', user_params.deep_merge(user: { service: 2 })
       post '/users/sign_in', user_params.deep_merge(user: { service: 2, remember_me: 0 })
       expect(response.status).to eq 302
-      expect(flash[:alert]).to be_include '本登録を行ってください。'
+      expect(flash[:alert]).to be_include I18n.t 'devise.failure.unconfirmed'
     end
   end
 
-  context 'POST /users (ユーザー登録) when need mail registration ' do
+  context 'POST /users (ユーザー登録) when need mail registration' do
     it 'メール認証が通ること' do
       post '/users', user_params.deep_merge(user: { service: 2 })
       mail_id = User.first.uid - 1
       authenticate_url = URI.extract(ActionMailer::Base.deliveries[mail_id].body.raw_source, ['http']).first.to_s
       get authenticate_url
-      expect(flash[:notice]).to eq 'アカウント登録が完了しました。'
+      expect(flash[:notice]).to eq I18n.t 'devise.confirmations.confirmed'
     end
   end
 
-  context 'POST /users (ユーザー登録) when need mail registration ' do
+  context 'POST /users (ユーザー登録) when need mail registration' do
     it 'メール認証が通りログイン出来ること' do
       post '/users', user_params.deep_merge(user: { service: 2 })
       mail_id = User.first.uid - 1
@@ -59,9 +59,13 @@ RSpec.describe 'Users Registrations', type: :request do
       rsa_public = OpenSSL::PKey.read ENV['RSA_PUBLIC']
       session_data = JWT.decode JSON.parse(response.body)['token'], rsa_public, true, algorithm: 'RS256'
       expect(response.status).to eq 200
-      expect(session_data.first.deep_symbolize_keys[:email]).to eq 'username+1@basicinc.jp'
+      expect(session_data.first.deep_symbolize_keys[:email]).to eq user_params[:user][:email]
       expect(response).to match_response_schema('/users')
     end
+  end
+
+  context 'twitter omniauth 認証' do
+    
   end
 
   context 'POST /users (ユーザー登録) & user is persisted' do
@@ -88,8 +92,8 @@ RSpec.describe 'Users Registrations', type: :request do
       patch '/users', user: { email: 'changed@basicinc.jp', password: 'password',
                               password_confirmation: 'password', current_password: 'password' }
       expect(response.status).to eq 204
-      expect(User.first.email1).to eq('changed@basicinc.jp')
-      expect(flash[:notice]).to eq 'アカウント情報を更新しました。'
+      expect(User.first.email1).to eq 'changed@basicinc.jp'
+      expect(flash[:notice]).to eq I18n.t 'devise.registrations.updated'
     end
   end
 
