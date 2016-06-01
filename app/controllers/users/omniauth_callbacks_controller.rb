34ae84eb37d7038d05e2c360218d363f8c08d84b
yosuke0315 <wonderfulworld0315@gmail.com>
2016-06-01 18:06:08 +0900

    twitter認証した際に、serviceによってtwitter_idかid_twitterを返す

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,10 +1,23 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   # You should configure your model like this:
   # devise :omniauthable, omniauth_providers: [:twitter]
+  # alias_method :twitter, :all
+  #
+  # def all
+  #   binding.pry
+  # end
 
   # You should also create an action method in this controller like this:
-  # def twitter
-  # end
+  def all
+    service_params = request.env['omniauth.params']['service'].to_i
+    auth = request.env['omniauth.auth']
+    if check_omniauth(auth, service_params)
+      response_omniauth(auth, service_params)
+    else
+      flash[:alert] = I18n.t 'errors.messages.already_confirmed'
+      render(json: flash.to_hash, status: :ok) && return
+    end
+  end
 
   # More info at:
   # https://github.com/plataformatec/devise#omniauth
@@ -18,6 +31,27 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   # def failure
   #   super
   # end
+
+  private
+
+  alias twitter all
+  alias facebook all
+
+  def check_omniauth(auth, service_params)
+    if service_params == 2
+      User.where("id_#{auth.provider}".to_sym == auth.uid).empty?
+    else
+      User.where("#{auth.provider}_id".to_sym == auth.uid).empty?
+    end
+  end
+
+  def response_omniauth(auth, service_params)
+    if service_params == 2
+      render(json: { "id_#{auth.provider}".to_sym => auth.uid }, status: :ok) && return
+    else
+      render(json: { "#{auth.provider}_id".to_sym => auth.uid }, status: :ok) && return
+    end
+  end
 
   # protected
 
