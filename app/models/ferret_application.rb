eacc755f21d9040480f3e7a8dc4be3b6946d68ff
yosuke0315 <wonderfulworld0315@gmail.com>
2016-06-22 17:24:53 +0900

    tiny fix

--- app/models/ferret_application.rb
+++ app/models/ferret_application.rb
@@ -4,28 +4,28 @@ class FerretApplication
       2
     end
 
-    def plus_url
-      'https://tool.ferret-plus.com'
-    end
-
     def media
       4
-    end
-
-    def media_url
-      'https://ferret-plus.com'
     end
 
     def marketers_store
       6
     end
 
-    def marketers_store_url
-      'https://mkst.ferret-plus.com'
-    end
-
     def contents_writing
       7
+    end
+
+    def plus_url
+      'https://tool.ferret-plus.com'
+    end
+
+    def media_url
+      'https://ferret-plus.com'
+    end
+
+    def marketers_store_url
+      'https://mkst.ferret-plus.com'
     end
 
     def contents_writing_url
