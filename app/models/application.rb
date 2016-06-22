7fcac80cb1bcccff06416f6dc1126dcc44851d25
yosuke0315 <wonderfulworld0315@gmail.com>
2016-06-22 16:58:15 +0900

    Settingsから各アプリケーションのURLを削除 & モデルに各アプリケーションのURLを追加

---
+++ app/models/application.rb
@@ -0,0 +1,19 @@
+class Application
+  class << self
+    def plus_url
+      'https://tool.ferret-plus.com'
+    end
+
+    def media_url
+      'https://ferret-plus.com'
+    end
+
+    def marketers_store_url
+      'https://mkst.ferret-plus.com'
+    end
+
+    def contents_writing_url
+      'https://cw.ferret-plus.com'
+    end
+  end
+end
